1d82efe2aa6886113f77f32af862a60a719cd754
Shash777 <shashjags@Gmail.com>
2021-04-22 19:12:02 +0530

    second commit

--- src/components/dash-page/dash-page.component.ts
+++ src/components/dash-page/dash-page.component.ts
@@ -13,6 +13,24 @@ export class DashPageComponent implements OnInit {
     let rec1 = localStorage.getItem("rec", )
   }
   ngOnInit(): void {
+
   }
 
+ c1=0;
+ inc = 0;
+ dec= 0;
+  Count(){
+    this.c1 = this.c1+1;
+    console.log(this.c1)
+  }
+
+
+  Inc() {
+     this.inc = this.c1+1;
+  }
+
+  Dec(){
+    this.dec = this.c1 -1;
+  }
 }
+
